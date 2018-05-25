eae718f708b859bf66d0416531dec951338a390a
Kenny Lam <kmlam@ucdavis.edu>
2018-05-25 14:15:18 -0700

    add seedBlogData function

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -12,3 +12,26 @@ const {app, runServer, closeServer} = require ('../server')
 const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
+
+//seed data
+function seedBlogData(){
+    console.info('seeding blog data');
+    const seedData = [];
+
+    for (let i = 1; i<=10; i++){
+        seedData.push(generateBlogPost());
+    }
+    return BlogPost.insertMany(seedData);
+}
+
+function generateBlogPost(){
+    return {
+        author: {
+            firstName: faker.name.firstName ,
+            lastName:  faker.name.lastName
+            },
+        title: faker.name.title, 
+        content: faker.lorem.paragraph,
+        created: faker.date.recent
+    }
+}
