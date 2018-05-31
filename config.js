815a36dfdfffea3a2b8a7f0b9be75b269abf5825
Kenny Lam <kmlam@ucdavis.edu>
2018-05-30 17:13:46 -0700

    configure heroku and travis

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/blog-app-test';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://kmlam:testing123@ds135540.mlab.com:35540/blogpost-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://kmlam:testing123@ds139970.mlab.com:39970/blogpost-test';
 exports.PORT = process.env.PORT || 8080;
